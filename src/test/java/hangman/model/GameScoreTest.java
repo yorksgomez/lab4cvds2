055c4ca7f9484aede0bae7419b137a6c3ef62d85
unknown <yorks.gomez@labinfo.is.escuelaing.edu.co>
2022-09-03 09:46:19 -0500

    implementacion de pruebas

---
+++ src/test/java/hangman/model/GameScoreTest.java
@@ -0,0 +1,141 @@
+package hangman.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Checks game score class is working correctly
+ * Also tests using the classes OriginalScore, BonusScore, PowerScore
+ */
+public class GameScoreTest {
+
+    @Test
+    public void Given_6Correct4Incorrect_When_OriginalScore_Then_ShouldReturn60() {
+        //Arrage
+        int expected = 60;
+        int testingCorrect = 6,
+            testingIncorrect = 4;
+
+        //Action
+        int actual = new OriginalScore().calculateScore(testingCorrect, testingIncorrect);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Given_6Correct100Incorrect_When_OriginalScore_Then_ShouldReturn60() {
+        //Arrage
+        int expected = 60;
+        int testingCorrect = 100,
+                testingIncorrect = 4;
+
+        //Action
+        int actual = new OriginalScore().calculateScore(testingCorrect, testingIncorrect);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Given_0Correct0Incorrect_When_OriginalScore_Then_ShouldReturn0() {
+        //Arrage
+        int expected = 0;
+        int testingCorrect = 0,
+                testingIncorrect = 0;
+
+        //Action
+        int actual = new OriginalScore().calculateScore(testingCorrect, testingIncorrect);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Given_6Correct4Incorrect_When_BonusScore_Then_ShouldReturn40() {
+        //Arrage
+        int expected = 40;
+        int testingCorrect = 6,
+                testingIncorrect = 4;
+
+        //Action
+        int actual = new BonusScore().calculateScore(testingCorrect, testingIncorrect);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Given_6Correct100Incorrect_When_BonusScore_Then_ShouldReturn0() {
+        //Arrage
+        int expected = 0;
+        int testingCorrect = 6,
+                testingIncorrect = 100;
+
+        //Action
+        int actual = new BonusScore().calculateScore(testingCorrect, testingIncorrect);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Given_0Correct0Incorrect_When_BonusScore_Then_ShouldReturn0() {
+        //Arrage
+        int expected = 0;
+        int testingCorrect = 0,
+                testingIncorrect = 0;
+
+        //Action
+        int actual = new BonusScore().calculateScore(testingCorrect, testingIncorrect);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Given_6Correct4Incorrect_When_PowerScore_Then_ShouldReturn19498() {
+        //Arrage
+        int expected = 19498;
+        int testingCorrect = 6,
+                testingIncorrect = 4;
+
+        //Action
+        int actual = new PowerScore().calculateScore(testingCorrect, testingIncorrect);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Given_1Correct4Incorrect_When_PowerScore_Then_ShouldReturn0() {
+        //Arrage
+        int expected = 0;
+        int testingCorrect = 1,
+                testingIncorrect = 4;
+
+        //Action
+        int actual = new PowerScore().calculateScore(testingCorrect, testingIncorrect);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Given_0Correct0Incorrect_When_PowerScore_Then_ShouldReturn0() {
+        //Arrage
+        int expected = 0;
+        int testingCorrect = 0,
+                testingIncorrect = 0;
+
+        //Action
+        int actual = new PowerScore().calculateScore(testingCorrect, testingIncorrect);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+}
